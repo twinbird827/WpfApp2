0089e651339ee8c1eff2fafda0839d1fd8826e83
twinbird827 <twinbird827@gmail.com>
2018-01-31 17:52:18 +0900

    [new] TODO

---
+++ Line.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    public class Line
+    {
+        public double X { get; set; }
+
+        public double Y { get; set; }
+    }
+}
---
+++ Series.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace WpfApp2
+{
+    public class Series
+    {
+        public double MinX { get; set; }
+
+        public double MaxX { get; set; }
+
+        public double MinY { get; set; }
+
+        public double MaxY { get; set; }
+
+        public Color LineColor { get; set; }
+
+        public IEnumerable<Line> Lines { get; set; }
+    }
+}
